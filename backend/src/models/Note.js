2b9d4af156fb675505b7a4d8ce6c7c60f16d7474
aamiral1 <amohamedthahirali1@sheffield.ac.uk>
2026-01-04 00:40:56 +0530

    set up database connection

---
+++ backend/src/models/Note.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type:String,
+        require: true
+    },
+    
+    content: {
+        type: String,
+        require: true
+    }
+},
+{ timestamps: true})
