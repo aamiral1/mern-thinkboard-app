6b31ffea3c6c83ced273dfbb168fcadd4fb1b74b
aamiral1 <amohamedthahirali1@sheffield.ac.uk>
2026-01-05 00:15:14 +0530

    implemented rate limiter

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,14 @@
+import ratelimit from "../config/upstash.js";
+
+const rateLimiter = async (req,res,next) => {
+    try {
+        const { success } = await ratelimit.limit("mylimitkey");
+        if (!success) return res.status(429).json({message: "Too many requests, please try again later."});
+        next();
+    } catch (error) {
+        console.log("Error in Rate Limiter", error);
+        next();
+    }
+}
+
+export default rateLimiter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 import dotenv from "dotenv"
 
 dotenv.config()
 
 const app = express();
 
-connectDB();
 app.use(express.json());
+app.use(rateLimiter);
+
 app.use("/api/notes", notesRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log("Server started on PORT:", process.env.PORT);
-});
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("Server started on PORT:", process.env.PORT);
+    });
+})
